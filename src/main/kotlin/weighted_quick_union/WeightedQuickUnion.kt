1c7c489a31ca0c3af61bc2f76611d651ca9ba851
Nikolay Miroshnychenko <nsmirosh@gmail.com>
2022-03-29 00:40:32 +0200

    changes

---
+++ src/main/kotlin/weighted_quick_union/WeightedQuickUnion.kt
@@ -0,0 +1,68 @@
+package weighted_quick_union
+
+class WeightedQuickUnion(n: Int) {
+
+    private var ids = Array(10) { i -> i }
+
+    init {
+        for (i in 0 until n) {
+            ids[i] = i
+        }
+    }
+
+    fun connected(q: Int, p: Int) : Boolean {
+
+        val result = root(p) == root(q)
+
+        println("$q and $p are connected = $result, because root of $q is ${root(q)} and root of $p is ${root(p)}")
+
+        return result
+    }
+
+
+        /*
+         chase pointers until you reach the root
+       */
+        private fun root(i: Int): Int {
+
+            var currentPos = i
+            while (currentPos != ids[currentPos]) {
+                currentPos = ids[currentPos]
+            }
+            return currentPos
+        }
+
+
+        /*
+        Change root of P to point to root of Q
+        */
+        fun union(q: Int, p: Int) {
+
+            println("union of $q and $p")
+
+            val pRoot = root(p)
+            val qRoot = root(q)
+
+            ids[pRoot] = qRoot
+
+        }
+
+
+        fun printStatus() {
+            println()
+            print(" ")
+
+            ids.forEachIndexed { i, item->
+                print("$i  ")
+            }
+            println()
+            print("[")
+            ids.forEach {
+                print("$it, ")
+            }
+            println("]")
+
+            println()
+
+        }
+}
